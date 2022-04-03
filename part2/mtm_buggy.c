ae556d06c2fb5bfdf51b2c60a0ab00731b817b16
idanmagram <idan.magram@gmail.com>
2022-04-03 23:54:51 +0300

    fix error 2 - words size (line 77)

--- part2/mtm_buggy.c
+++ part2/mtm_buggy.c
@@ -74,7 +74,7 @@ void printResults(char** words, int size) {
 	char* longest = getLongestString(words, size);
 	printf("The longest word is: %s\n", longest);
 	sortStrings(words, size);
-	printf("The maximal word lexicographically is: %s\n", words[size]);
+	printf("The maximal word lexicographically is: %s\n", words[size-1]);
 	printf("The minimal word lexicographically is: %s\n", words[0]);
 }
 
